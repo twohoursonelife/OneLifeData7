b19f50ede9ec6a409fe9c97be712b93f671314ac
risvh <67486979+risvh@users.noreply.github.com>
2024-03-19 01:09:39 +0800

    Create readableAssets.py

---
+++ readableAssets.py
@@ -0,0 +1,20 @@
+import os
+import uuid
+
+input_changes = os.environ.get("INPUT_CHANGES")
+
+
+def set_multiline_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+
+
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
+        
+        
+set_output("OUTPUT_CHANGES", "TESTING " + str(input_changes))
